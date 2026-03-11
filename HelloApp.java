788046314c627d74856fb8aaf8ebfda0e1c83a19
rajesh-102007 <ra2735@srmist.edu.in>
2026-03-11 15:34:19 +0530

    Added first program

---
+++ HelloApp.java
@@ -0,0 +1,10 @@
+/**
+ * @author [A Rajesh Kanna]
+ * @version UC1: Display "Hello World"
+ */   
+public class HelloApp {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
