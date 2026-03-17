d5d1f1740a8b52179e58c161fbff16aa421b8b53
rajesh-102007 <ra2735@srmist.edu.in>
2026-03-17 14:09:40 +0530

    Added fourth program

--- HelloApp.java
+++ HelloApp.java
@@ -2,11 +2,23 @@
  * @author [A Rajesh Kanna]
  * @version 1.0 UC3
  */
-class UseCase3
-{
-    public static void main(String args[])
-    {
-        String name = (args.length > 0) ? args[0] : "World";
-        System.out.println("Hello, "+name+"!");
+class UseCase4 {
+    public static void main(String args[]) {
+        String name;
+        StringBuilder nameBuilder = new StringBuilder();
+
+        if (args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            name = nameBuilder.toString();
+        } else {
+            name = "World"; // Default message
+        }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
