8ee21c54edb0b678d6108a3cff5edeea075938a5
rajesh-102007 <ra2735@srmist.edu.in>
2026-03-18 14:31:42 +0530

    Added fifth program

--- HelloApp.java
+++ HelloApp.java
@@ -1,19 +1,19 @@
-/**
- * @author [A Rajesh Kanna]
- * @version 1.0 UC3
- */
-class UseCase4 {
-    public static void main(String args[]) {
+public class UseCase5 {
+    public static void main(String[] args) {
         String name;
-        StringBuilder nameBuilder = new StringBuilder();
 
         if (args.length > 0) {
-            for (int i = 0; i < args.length; i++) {
-                nameBuilder.append(args[i]);
-                if (i < args.length - 1) {
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+
+            for (String n : args) {
+                if (!first) {
                     nameBuilder.append(", ");
                 }
+                nameBuilder.append(n);
+                first = false;
             }
+
             name = nameBuilder.toString();
         } else {
             name = "World"; // Default message
